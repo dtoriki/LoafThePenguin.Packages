756f5ab1c920c78219edf198dc6c3a03efcee78b
dtoriki <dev.toriki@gmail.com>
2022-12-10 19:10:53 +0500

    (feat) Добавлен поиск даты дня недели.

--- src/LoafThePenguin.Helpers/src/LoafThePenguin.Helpers/DateOnlyHelper.cs
+++ src/LoafThePenguin.Helpers/src/LoafThePenguin.Helpers/DateOnlyHelper.cs
@@ -9,4 +9,39 @@ public static class DateOnlyHelper
     /// Возвращает текущую дату.
     /// </summary>
     public static DateOnly DateToday => DateOnly.FromDateTime(DateTime.Now);
+
+    /// <summary>
+    /// Ищет и возвращает следующую дату дня недели.
+    /// </summary>
+    /// <param name="dayOfWeek">День недели, дату которого нужно найти.</param>
+    /// <returns>Дата дня недели.</returns>
+    public static DateOnly NextDayOfWeekDate(DayOfWeek dayOfWeek)
+    {
+        return DayOfWeekDate(dayOfWeek, nextDayOfweek: true);
+    }
+
+    /// <summary>
+    /// Ищет и возвращает предыдущую дату дня недели.
+    /// </summary>
+    /// <param name="dayOfWeek">День недели, дату которого нужно найти.</param>
+    /// <returns>Дата дня недели.</returns>
+    public static DateOnly PrevDayOfWeekDate(DayOfWeek dayOfWeek)
+    {
+        return DayOfWeekDate(dayOfWeek, nextDayOfweek: false);
+    }
+
+    private static DateOnly DayOfWeekDate(DayOfWeek dayOfWeek, bool nextDayOfweek)
+    {
+        int direction = nextDayOfweek
+            ? 1
+            : -1;
+
+        DateOnly result = DateToday.AddDays(direction);
+        while(result.DayOfWeek != dayOfWeek)
+        {
+            result = DateToday.AddDays(direction);
+        }
+
+        return result;
+    }
 }
